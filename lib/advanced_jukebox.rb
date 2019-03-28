6a8e2f604a3a2eb65eba177147d792738120ac99
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-28 20:51:27 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -14,7 +14,13 @@
 
 def help
   #this method should be the same as in jukebox.rb
-
+  if gets.chomp == 'help'
+    puts "I accept the following commands:
+  - help : displays this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program"
+  end
 end
 
 
@@ -22,6 +28,13 @@ end
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
   #list the songs by name
+  puts my_songs
+  
+  my_songs.each_with_index do |el, i|
+    if gets.chomp == (i+1) 
+      puts el
+    end
+  end
 end
 
 
@@ -33,13 +46,37 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+  puts "Please enter a song name or number:"
+  gets.chomp
+  if (my_songs.keys.include?(gets.chomp))
+    puts "Playing #{gets.chomp}"
+    my_songs[gets.chomp]
+  else
+    puts "Invalid input, please try again"
+  end
 end
 
 def exit_jukebox
   #this method is the same as in jukebox.rb
+  puts "Goodbye" if (gets.chomp == 'exit')
 end
 
 def run(my_songs)
   #this method is the same as in jukebox.rb
+  until gets.chomp == 'exit'
+  help
+  puts "Please enter a command:"
+  gets.chomp
+    if gets.chomp == 'list'
+      list
+    else if gets.chomp == 'play'
+      play
+    else if gets.chomp == 'exit' 
+      exit_jukebox
+      break
+    end
+  end
+  if gets.chomp == 'exit' 
+      exit_jukebox
+  end
 end
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,67 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  if gets.chomp == 'help'
+    puts "I accept the following commands:
+  - help : displays this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program"
+  end
+end
+
+
+
+def list(songs)
+  puts songs
+  puts "1. Phoenix - 1901
+2. Tokyo Police Club - Wait Up
+3. Sufjan Stevens - Too Much
+4. The Naked and the Famous - Young Blood
+5. (Far From) Home - Tiga
+6. The Cults - Abducted
+7. Phoenix - Consolation Prizes
+8. Harry Chapin - Cats in the Cradle
+9. Amos Lee - Keep It Loose, Keep It Tight"
+
+  songs.each_with_index do |el, i|
+    if gets.chomp == (i+1) 
+      puts el
+    end
+  end
+end
+
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  gets.chomp
+  if (my_songs.keys.include?(gets.chomp))
+    puts "Playing #{my_songs[gets.chomp}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye" if (gets.chomp == 'exit')
+end
+
+def run(my_songs)
+  until gets.chomp == 'exit'
+  help
+  puts "Please enter a command:"
+  gets.chomp
+    if gets.chomp == 'list'
+      list
+    else if gets.chomp == 'play'
+      play
+    else if gets.chomp == 'exit' 
+      exit_jukebox
+      break
+    end
+  end
+  if gets.chomp == 'exit' 
+      exit_jukebox
+  end
+end
